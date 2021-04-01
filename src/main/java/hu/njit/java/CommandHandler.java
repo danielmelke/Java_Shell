1e0d4e8d9b879278810c4ada69263d46b24ccf34
Daniel Melke <daniel.melke13@gmail.com>
2021-04-01 17:54:10 +0200

    First upload
    Uploading all the files

---
+++ src/main/java/hu/njit/java/CommandHandler.java
@@ -0,0 +1,154 @@
+package hu.njit.java;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CommandHandler implements Runnable{
+
+    static String commandPass;
+
+    static boolean dirChanged = false;
+
+    static boolean isCommandIn(String command) {
+        return command.startsWith("cd") || command.startsWith("echo") || command.startsWith("ls");
+    }
+
+    public CommandHandler(String command) {
+        commandPass = command;
+    }
+
+    static void readFromProcess(Process process) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line = "";
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+    }
+
+    static void executeOut(String command) throws IOException, InterruptedException {
+        Process process = Runtime.getRuntime().exec("cmd /c " + command);
+        readFromProcess(process);
+        if (process.waitFor() != 0) {
+            System.out.println("Command did not execute properly");
+        }
+        Shell.prompt();
+    }
+
+    static void executeIn(String command) throws IOException{
+        if (command.contains(" | ")){
+            String firstCommand = command.split("\\|")[0];
+            String secondCommand = command.split("\\|")[1];
+            if (firstCommand.startsWith("echo")) {
+                String line = command.split("\\|")[0].substring(5);
+                System.out.println(line);
+                if (secondCommand.startsWith(" grep") || secondCommand.startsWith("grep")){
+                    System.out.println("---- grep ----");
+                    String toMatch = secondCommand.substring(5).replaceFirst(" ", "");
+                    if (line.contains(toMatch.toLowerCase()) || line.contains(toMatch.toUpperCase())){
+                        System.out.println(line);
+                    } else {
+                        System.out.println("grep: no match found");
+                    }
+                } else {
+                    System.out.println("Unknown second command");
+                }
+            } else if (firstCommand.replaceFirst(" ", "").equals("ls")) {
+                File f = new File(Shell.directory);
+                String[] dirNames = f.list();
+                for (int i = 0; i < dirNames.length; ++i) {
+                    System.out.print(dirNames[i] + "    ");
+                    if (i > 0 && i%3 == 0) {
+                        System.out.print("\n");
+                    }
+                }
+                System.out.print("\n");
+                if (secondCommand.startsWith(" grep") || secondCommand.startsWith("grep")){
+                    System.out.println("---- grep ----");
+                    String toMatch = secondCommand.substring(5).replaceFirst(" ", "");
+                    int matches = 0;
+                    for (int i = 0; i < dirNames.length; ++i) {
+                        if (dirNames[i].contains(toMatch.toLowerCase()) || dirNames[i].contains(toMatch.toUpperCase())){
+                            System.out.println(dirNames[i]);
+                            matches++;
+                        }
+                    }
+                    if (matches == 0) {
+                        System.out.println("grep: no match found");
+                    }
+                } else {
+                    System.out.println("Unknown second command");
+                }
+            }
+        } else {
+            if (command.startsWith("echo")) {
+                String line = command.substring(5);
+                System.out.println(line);
+            } else if (command.startsWith("cd")){
+                if (command.equals("cd ..")) {
+                    if (Shell.directory.length() >= 5) {
+                        int index = Shell.directory.lastIndexOf("\\");
+                        Shell.directory = Shell.directory.substring(0, index);
+                    }
+                } else {
+                    if (command.length() < 4) {
+                        System.out.println("Command did not execute properly");
+                    }
+                    else {
+                        File f = new File(Shell.directory + "\\" + command.substring(3));
+                        if (f.isDirectory()) {
+                            Shell.directory = Shell.directory + "\\" + command.substring(3);
+                        } else {
+                            System.out.println("Directory not found!");
+                        }
+                    }
+                }
+            } else if (command.equals("ls")) {
+                File f = new File(Shell.directory);
+                String[] dirNames = f.list();
+                for (int i = 0; i < dirNames.length; ++i) {
+                    System.out.print(dirNames[i] + "    ");
+                    if (i > 0 && i%3 == 0) {
+                        System.out.print("\n");
+                    }
+                }
+                System.out.print("\n");
+            }
+        }
+        Shell.prompt();
+    }
+
+    static void executeDirChanged(String command, String dir) throws IOException, InterruptedException {
+        if (isCommandIn(command)) {
+            executeIn(command);
+        } else {
+            Process process = Runtime.getRuntime().exec("cmd /c " + command, null, new File(dir));
+            readFromProcess(process);
+            if (process.waitFor() != 0) {
+                System.out.println("Command did not execute properly");
+            }
+            Shell.prompt();
+        }
+    }
+
+    @Override
+    public void run() {
+        if (!Shell.directory.equals(Shell.defaultDir)) {
+            dirChanged = true;
+        }
+        try {
+            if (dirChanged) {
+                executeDirChanged(commandPass, Shell.directory);
+            } else {
+                if ((isCommandIn(commandPass))) {
+                    executeIn(commandPass);
+                } else {
+                    executeOut(commandPass);
+                }
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/hu/njit/java/Shell.java
@@ -0,0 +1,47 @@
+package hu.njit.java;
+
+import java.awt.event.KeyEvent;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Shell {
+    static Path path = Paths.get("").toAbsolutePath();
+    static String directory = path.toString();
+    static String defaultDir = path.toString();
+
+    static String welcomeMessage = "\u001B[32m" +
+            "###### SHELL by Melke Daniel ######\n" +
+            "######## version 1.0, 2021 ########\n" +
+            "built-in commands: cd, ls, echo\n" +
+            "type exit to stop the program" +
+            "\u001B[0m";
+
+    static void prompt() {
+        System.out.print(directory + "\\# ");
+    }
+
+    static void userInput() throws IOException {
+        while (true) {
+            prompt();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String line;
+            while ((line = reader.readLine()) != null){
+                if (line.equals("exit")) {
+                    System.exit(0);
+                } else {
+                    CommandHandler commandHandler = new CommandHandler(line);
+                    Thread t = new Thread(commandHandler);
+                    t.start();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(welcomeMessage);
+        userInput();
+    }
+}
